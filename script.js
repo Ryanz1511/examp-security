d6c120c4b5e7790af9b10e3dd985eac2985a9e34
Muhammad Ryan <mhmmdryan2392@gmail.com>
2025-08-26 17:58:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener("selectstart", e => e.preventDefault());
 
 const target = document.body;
 
+// PC
 document.addEventListener("keydown", e => {
   if (
     e.key === "F12" ||
@@ -21,16 +22,18 @@ document.addEventListener("keydown", e => {
   }
 });
 
+// Minimize 
 document.addEventListener("visibilitychange", () => {
   if (document.hidden) {
     alert("Anda meninggalkan halaman ujian!");
+    target.style.display = "none";
+    setTimeout(() => {
+      alert("Akses dilarang!");
+      target.style.display = "block";
+    }, 300);
   }
-  target.style.display = "none";
-  setTimeout(() => {
-    alert("Akses dilarang!");
-    target.style.display = "block";
-  }, 300);
 });
+
 
 let touchBlocked = false;
 let startY = 0;
@@ -42,43 +45,12 @@ document.addEventListener("touchstart", e => {
 document.addEventListener("touchmove", e => {
   const dy = e.touches[0].clientY - startY;
   const atTop = window.scrollY <= 0;
+
   if (atTop && dy > 80) {
     e.preventDefault();
     if (!touchBlocked) {
       touchBlocked = true;
       target.style.display = "none";
-      setTimeout(() => {
-        alert("Akses dilarang!");
-        target.style.display = "block";
-        setTimeout(() => (touchBlocked = false), 1500);
-      }, 200);
-    }
-  }
-}, { passive: false });
-      target.style.display = "block";
-    }, 300);
-  
-});
-
-// Android
-let touchBlocked = false;
-let startY = 0;
-
-document.addEventListener("touchstart", e => {
-  startY = e.touches[0].clientY;
-}, { passive: true });
-
-document.addEventListener("touchmove", e => {
-  const dy = e.touches[0].clientY - startY; 
-  const atTop = window.scrollY <= 0;
-
-
-    e.preventDefault();
-
-    if (!touchBlocked) {
-      touchBlocked = true;
-      target.style.display = "none";
-
       setTimeout(() => {
         alert("Akses dilarang!");
         target.style.display = "block";
